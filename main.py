5834528963b2453c20eb0fb9feec356b21e2de5e
Rifah Solkar <rmsolkar98@gmail.com>
2023-07-21 11:56:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ def create_blog(request: schemas.Blog , db:Session = Depends(get_db)):
 
 # changing port
 
-# if __name__ =='__main__':
-#     uvicorn.run(app,host='127.0.0.1',port='9000')
+if __name__ =='__main__':
+    uvicorn.run(app,host='127.0.0.1',port='9000')
